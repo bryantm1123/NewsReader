7920e124f70ddb7a1551136f8309c685371f6e53
Matt <bryantm1122@gmail.com>
2025-03-04 18:06:29 +0100

    adding some documentation for the rest client and rest url session components

--- GoogleNewsDemoApp/Shared/RestService/RestClient.swift
+++ GoogleNewsDemoApp/Shared/RestService/RestClient.swift
@@ -2,21 +2,27 @@ import Foundation
 
 public typealias RestClientResponse = (Data, URLResponse)
 
+/// Represents a client for RESTful network transactions.
 public protocol RestClientProtocol {
+    /// Executes a REST request asynchronously.
+    /// - Parameter request: A request  for which to load data — conforming to ``RestRequesting``.
+    /// - Returns: ``RestClientResponse`` — a tuple of type [Data](https://developer.apple.com/documentation/foundation/data) and [URLResponse](https://developer.apple.com/documentation/foundation/urlresponse).
     func execute<R: RestRequesting>(request: R) async throws -> RestClientResponse
 }
 
+/// An implementation of the ``RestClientProtocol``.
 public final class RestClient: RestClientProtocol {
     
+    /// Represents a URLSession
     private var session: RestURLSessionProtocol
     
+    /// Public initializer for the ``RestClient``.
+    /// - Parameter session: a session upon which to execute network requests — defaults to the shared URLSession singleton.
     public init(session: RestURLSessionProtocol = URLSession.shared) {
         self.session = session
     }
     
-    /// Executes a request asynchronously.
-    /// - Parameter request: A request conforming to `RestRequesting` for which to load data.
-    /// - Returns: type `RestClientResponse` which is a tuple of type `Data` and `URLResponse`.
+    /// Implements the ``RestClientProtocol`` `execute(request:)` method.
     public func execute<R: RestRequesting>(request: R) async throws -> RestClientResponse {
         switch request.method {
         case .GET:
--- GoogleNewsDemoApp/Shared/RestService/RestURLSession.swift
+++ GoogleNewsDemoApp/Shared/RestService/RestURLSession.swift
@@ -1,6 +1,13 @@
 import Foundation
 
+/// Represents a URLSession
 public protocol RestURLSessionProtocol {
+    
+    /// Mimics the [data(for:delegate:)](https://developer.apple.com/documentation/foundation/urlsession/3767352-data) method on URLSession for the purpose of creating a mock of URLSession for testing purposes.
+    /// - Parameters:
+    ///   - request: a request for which to load data.
+    ///   - delegate: a delegate that receives life-cycle and authentication challenges.
+    /// - Returns:  a tuple of type [Data](https://developer.apple.com/documentation/foundation/data) and [URLResponse](https://developer.apple.com/documentation/foundation/urlresponse).
     func data(for request: URLRequest, delegate: (any URLSessionTaskDelegate)?) async throws -> (Data, URLResponse)
 }
 
